26b0513472d5dd588bb9b6abcdd5878f34398441
Julia_Vladomira <juliavladomira@gmail.com>
2024-01-07 22:42:01 +0100

    add loading elemnt

--- src/components/Fallback/index.tsx
+++ src/components/Fallback/index.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 
+const dots = [1, 2, 3, 4, 5, 6];
 function Fallback() {
   return (
     <div
       className="w-screen bg-black py-16"
       style={{ minHeight: "calc(100vh - 53px)" }}
     >
+      {" "}
+      <div className="flex justify-center items-center mb-7">
+        {dots.map((el) => (
+          <div
+            key={el}
+            className="wave w-[5px] h-9 m-1 rounded-2xl drop-shadow-shiny"
+          />
+        ))}
+      </div>
       <div
         className={`m-auto bg-[url(/assets/gifs/rick-dancing.gif)]  w-[400px] h-[400px]  bg-contain bg-no-repeat`}
       />
